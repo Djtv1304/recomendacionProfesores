a1212654d0390b91023846b2b33def557e6685bd
Djtv1304 <diegosebastian_t@hotmail.com>
2024-07-06 15:55:37 -0500

    Implementación de Eureka para uso del Middleware

--- src/main/java/com/microservicio/recomendacionProfesores/Controller/ProfesorController.java
+++ src/main/java/com/microservicio/recomendacionProfesores/Controller/ProfesorController.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/recomendacionProfesores")
 public class ProfesorController {
 
     ProfesorService profesorService;
--- src/main/java/com/microservicio/recomendacionProfesores/RecomendacionProfesoresApplication.java
+++ src/main/java/com/microservicio/recomendacionProfesores/RecomendacionProfesoresApplication.java
@@ -2,10 +2,12 @@ package com.microservicio.recomendacionProfesores;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories
+@EnableDiscoveryClient
 public class RecomendacionProfesoresApplication {
 
 	public static void main(String[] args) {
